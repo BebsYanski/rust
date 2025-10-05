9535b5539be16151b796d279bddda7260b853b2a
BebsYanski <bebongnchuy@gmail.com>
2025-10-05 13:11:53 +0100

    new

--- Docs/functions/src/main.rs
+++ Docs/functions/src/main.rs
@@ -1,8 +1,19 @@
 fn main() {
     println!("Hello, world!");
-    another_function();
+    another_function(23);
+    let sum = add(5, 6);
+    println!("The sum is: {}", sum);
+    let x = five();
+    println!("The value of x is: {}", x);
 }
 
-fn another_function() {
+fn another_function(x: i32) {
     println!("Another function");
+    println!("The value of x is: {}", x);
+}
+fn add(x: i32, y: i32) -> i32 {
+    x + y
+}
+fn five() -> i32 {
+    5
 }
